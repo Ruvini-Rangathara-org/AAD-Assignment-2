c5b06dd4fc31c53bfd75d11d43a4e0771b312afd
Shan <perera.alc2000@gmail.com>
2023-09-23 23:48:18 +0530

    fixed

--- src/main/java/com/example/assignment2/repository/ProjectRepository.java
+++ src/main/java/com/example/assignment2/repository/ProjectRepository.java
@@ -4,14 +4,26 @@ import com.example.assignment2.entity.ProjectEntity;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-public interface ProjectRepository extends CrudRepository<ProjectEntity> {
+import java.util.List;
+
+@Repository
+public interface ProjectRepository extends org.springframework.data.jpa.repository.JpaRepository<ProjectEntity, Integer> {
     @Modifying
     @Transactional
     @Query("UPDATE project p SET p.title = :title, p.description = :description WHERE p.code = :code")
     void updateProjectById(@Param("code") Integer code,
                        @Param("title") String title,
                        @Param("description") String description);
+    List<ProjectEntity> findAll();
 
+    ProjectEntity getById(Integer integer);
+
+    ProjectEntity save(ProjectEntity entity);
+
+    long count();
+
+    void deleteById(Integer code);
 }
